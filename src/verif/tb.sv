1bfd16ba2621357f01e4826297378feca9b4e717
Ashley J. Robinson <ashley181291@gmail.com>
2023-07-15 16:39:24 +0100

    upgrade TB

--- src/verif/tb.sv
+++ src/verif/tb.sv
@@ -1,34 +1,33 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-module tb (
-);
+module tb ();
 
-initial begin
-$dumpfile ("tb.vcd");
-$dumpvars (0, tb);
-#1;
-end
-
-wire [7:0] uo_out;
-wire [7:0] ui_in;
-wire [7:0] uio_out;
-wire [7:0] uio_in;
-wire [7:0] uio_oe;
-wire clk;
-wire rst_n;
-wire ena;
-
-tt_um_ashleyjr_delay_line u_dut (
-.ui_in      (ui_in),    // Dedicated inputs
-.uo_out     (uo_out),   // Dedicated outputs
-.uio_in     (uio_in),   // IOs: Input path
-.uio_out    (uio_out),  // IOs: Output path
-.uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-.ena        (ena),      // enable - goes high when design is selected
-.clk        (clk),      // clock
-.rst_n      (rst_n)     // not reset
-);
+   initial begin
+      $dumpfile ("tb.vcd");
+      $dumpvars (0, tb);
+      #1;
+   end
+   
+   wire [7:0] uo_out;
+   wire [7:0] ui_in;
+   wire [7:0] uio_out;
+   wire [7:0] uio_in;
+   wire [7:0] uio_oe;
+   wire clk;
+   wire rst_n;
+   wire ena;
+   
+   tt_um_ashleyjr_delay_line u_dut (
+      .ui_in      (ui_in),    // Dedicated inputs
+      .uo_out     (uo_out),   // Dedicated outputs
+      .uio_in     (uio_in),   // IOs: Input path
+      .uio_out    (uio_out),  // IOs: Output path
+      .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena        (ena),      // enable - goes high when design is selected
+      .clk        (clk),      // clock
+      .rst_n      (rst_n)     // not reset
+   );
 
 endmodule
 
--- src/verif/test.py
+++ src/verif/test.py
@@ -5,3 +5,27 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
     dut._log.info("start")
+
+    # Setup 50MHz clock
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Reset design (active low)
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+
+    # Design has been selected
+    await ClockCycles(dut.clk, 1000)
+    dut.ena.value = 1
+
+    # Design has been selected
+    await ClockCycles(dut.clk, 1000)
+    dut.ena.value = 1
+
+    # UART is idle
+    await ClockCycles(dut.clk, 1000)
+    dut.ui_in.value = 0x01
+
+
+    await ClockCycles(dut.clk, 1000)
