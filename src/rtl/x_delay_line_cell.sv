4bceb270fe9449c54e31cc8bb436a289650559a7
Ashley J. Robinson <ashley181291@gmail.com>
2023-08-01 17:24:57 +0100

    try delay gate

--- src/rtl/x_delay_line_cell.sv
+++ src/rtl/x_delay_line_cell.sv
@@ -10,47 +10,11 @@ module x_delay_line_cell(
 
 `else
 
-   logic a;
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_a(
+   (* keep = "true" *) sky130_fd_sc_hd__dlygate4sd3_1 u_inv_a(
       .A(i_a),  
-      .Y(a)
-   );
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_b(
-      .A(a),  
-      .Y(b)
-   );
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_c(
-      .A(b),  
-      .Y(c)
-   );
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_d(
-      .A(c),  
-      .Y(d)
-   );
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_e(
-      .A(d),  
-      .Y(e)
-   );
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_f(
-      .A(e),  
-      .Y(f)
-   );
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_g(
-      .A(f),  
-      .Y(g)
-   );
-
-   (* keep = "true" *) sky130_fd_sc_hd__inv_1 u_inv_h(
-      .A(g),  
       .Y(o_y)
    );
+
 `endif
 
 endmodule
